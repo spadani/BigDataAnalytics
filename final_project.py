75d8ff72dfcf621e5bae4ea0e33b326d791327fe
Shezal Padani <shezal.padani@buoyhealth.com>
2021-04-19 20:57:21 -0400

    update branch key, add performance metrics, add model

--- final_project.py
+++ final_project.py
@@ -8,9 +8,11 @@ from pyspark import SparkContext
 from pyspark.ml.feature import StopWordsRemover
 from pyspark.sql import SQLContext
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
 from pyspark.mllib.regression import LabeledPoint
 from pyspark.mllib.tree import RandomForest
+from pyspark.sql.functions import concat, col, lit
+from pyspark.mllib.evaluation import MulticlassMetrics
+from pyspark.mllib.classification import LogisticRegressionWithLBFGS, LogisticRegressionModel, LogisticRegressionWithSGD
 
 nltk.download('stopwords')
 
@@ -40,7 +42,14 @@ def buildArray(listOfIndices):
 file = pd.read_csv("/Users/shezalpadani/Downloads/DisneylandReviews.csv")  # assuming the file contains a header
 df = sqlContext.createDataFrame(file)
 dfWithSchema = spark.createDataFrame(file).toDF("Review_ID", "Rating", "Year_Month", "Reviewer_Location", "Review_Text", "Branch")
+dfWithSchema = dfWithSchema.select(concat(col("Review_ID"), lit("-"), col("Branch")), col("Review_Text"))
+dfWithSchema = dfWithSchema.withColumnRenamed("concat(Review_ID, -, Branch)","Branch")
+
+print(dfWithSchema.show())
+
 dfWithSchema = dfWithSchema.select(col("Branch"),col("Review_Text"))
+print(dfWithSchema.show())
+
 rdd = dfWithSchema.rdd.map(tuple)
 numberOfDocs = rdd.count()
 print(numberOfDocs)
@@ -76,13 +85,11 @@ allDictionaryWords = dictionary.join(allWordsWithDocID)
 justDocAndPos = allDictionaryWords.map(lambda x: (x[1][1], x[1][0]))
 allDictionaryWordsInEachDoc = justDocAndPos.groupByKey()
 allDocsAsNumpyArrays = allDictionaryWordsInEachDoc.map(lambda x: (x[0], buildArray(x[1])))
-print(allDocsAsNumpyArrays.take(10))
 zeroOrOne = allDocsAsNumpyArrays.map(lambda x: (x[0], np.clip (np.multiply (x[1], 9e9), 0, 1)))
 dfArray = zeroOrOne.reduce(lambda x1, x2: ("", np.add(x1[1], x2[1])))[1]
 multiplier = np.full(20000, numberOfDocs)
 idfArray = np.log(np.divide(np.full(20000, numberOfDocs), dfArray))
 allDocsAsNumpyArrays = allDocsAsNumpyArrays.map(lambda x: (x[0], np.multiply(x[1], idfArray)))
-print(allDocsAsNumpyArrays.take(10))
 
 def convert(value):
 	if 'HongKong' in value:
@@ -96,32 +103,56 @@ def convert(value):
 # convert AU to 1 and wiki docs to 0
 myRDD = allDocsAsNumpyArrays
 test = myRDD.map(lambda x: x[0])
-print(test.take(1000))
 new_RDD = myRDD.map(lambda x: (convert(x[0]), x[1]))
 new_RDD = new_RDD.map(lambda x: LabeledPoint(x[0], x[1]))
+new_RDD.cache()
 
+print(new_RDD.count())
+# Split the data into training and test sets (30% held out for testing)
+trainingData, testData = new_RDD.randomSplit([.5, .5])
+print(trainingData.count())
+print(testData.count())
 
-# print(new_RDD.count())
-# # Split the data into training and test sets (30% held out for testing)
-# trainingData, testData = new_RDD.randomSplit([.5, .5])
-# print(trainingData.count())
-# print(testData.count())
-#
-# import psutl
-# # Train a RandomForest model.
-# #  Empty categoricalFeaturesInfo indicates all features are continuous.
-# #  Note: Use larger numTrees in practice.
-# #  Setting featureSubsetStrategy="auto" lets the algorithm choose.
+# Train a RandomForest model.
+#  Empty categoricalFeaturesInfo indicates all features are continuous.
+#  Note: Use larger numTrees in practice.
+#  Setting featureSubsetStrategy="auto" lets the algorithm choose.
 # model = RandomForest.trainClassifier(trainingData, numClasses=3, categoricalFeaturesInfo={},
-# 									 numTrees=10, featureSubsetStrategy="auto",
+# 									 numTrees=5, featureSubsetStrategy="auto",
 #                                      impurity='gini', maxDepth=5, maxBins=32)
-#
-# # Evaluate model on test instances and compute test error
-# predictions = model.predict(testData.map(lambda x: x.features))
-# labelsAndPredictions = testData.map(lambda lp: lp.label).zip(predictions)
-# print(labelsAndPredictions)
-# testErr = labelsAndPredictions.filter(
-#     lambda lp: lp[0] != lp[1]).count() / float(testData.count())
-# print('Test Error = ' + str(testErr))
-# print('Learned classification forest model:')
-# print(model.toDebugString())
+model = LogisticRegressionWithLBFGS.train(trainingData, numClasses=3)
+# Evaluate model on test instances and compute test error
+predictions = model.predict(testData.map(lambda x: x.features))
+labelsAndPredictions = testData.map(lambda lp: lp.label).zip(predictions)
+print(labelsAndPredictions.take(10))
+testErr = labelsAndPredictions.filter(
+    lambda lp: lp[0] != lp[1]).count() / float(testData.count())
+
+# Instantiate metrics object
+metrics = MulticlassMetrics(labelsAndPredictions)
+
+# Overall statistics
+precision = metrics.precision(1.0)
+recall = metrics.recall(1.0)
+f1Score = metrics.fMeasure(1.0)
+print("Summary Stats")
+print("Precision = %s" % precision)
+print("Recall = %s" % recall)
+print("F1 Score = %s" % f1Score)
+
+# Statistics by class
+labels = new_RDD.map(lambda lp: lp.label).distinct().collect()
+for label in sorted(labels):
+    print("Class %s precision = %s" % (label, metrics.precision(label)))
+    print("Class %s recall = %s" % (label, metrics.recall(label)))
+    print("Class %s F1 Measure = %s" % (label, metrics.fMeasure(label, beta=1.0)))
+
+# Weighted stats
+print("Weighted recall = %s" % metrics.weightedRecall)
+print("Weighted precision = %s" % metrics.weightedPrecision)
+print("Weighted F(1) Score = %s" % metrics.weightedFMeasure())
+print("Weighted F(0.5) Score = %s" % metrics.weightedFMeasure(beta=0.5))
+print("Weighted false positive rate = %s" % metrics.weightedFalsePositiveRate)
+print('Test Error = ' + str(testErr))
+print('Learned classification forest model:')
+print(model.toDebugString())
